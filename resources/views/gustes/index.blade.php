8fc0ced46351ce694624eef1773d8623b62c9f10
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-01-19 17:24:19 +0300

    update Langing page

--- resources/views/gustes/index.blade.php
+++ resources/views/gustes/index.blade.php
@@ -74,7 +74,7 @@
                         <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.1s">
                             <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                                 <div class="service-icon flex-shrink-0 ">
-                                    <i class="fa fa-home fa-2x"></i>
+                                    <i class="fas fa-tasks" style="font-size: 1.5rem"></i>
                                 </div>
                                 <h5 class="mb-3">تنظيم المهام</h5>
                                 <p>يقدم إنجاز واجهة سهلة لتنظيم المهام اليومية بشكل فعّال ومنظم، مما يجعل العمل أكثر إنتاجية.</p>
@@ -84,7 +84,7 @@
                         <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
                             <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                                 <div class="service-icon flex-shrink-0">
-                                    <i class="fa fa-home fa-2x"></i>
+                                    <i class="fas fa-hands-helping" style="font-size: 1.5rem" ></i>
                                 </div>
                                 <h5 class="mb-3">تعاون فعّال</h5>
                                 <p>يتيح إنجاز مشاركة المهام مع أعضاء الفريق وتحديد المسؤوليات بشكل واضح لتحقيق التعاون الفعّال.</p>
@@ -94,7 +94,7 @@
                         <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
                             <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                                 <div class="service-icon flex-shrink-0">
-                                    <i class="fa fa-home fa-2x"></i>
+                                    <i class="fas fa-cogs" style="font-size: 1.5rem"></i>
                                 </div>
                                 <h5 class="mb-3">أدوات متقدمة</h5>
                                 <p>تقدم إنجاز أدوات متقدمة لتحليل أداء المشروع وتحديد الأولويات لتحسين إدارة الوقت.</p>
@@ -104,7 +104,7 @@
                         <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.1s">
                             <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                                 <div class="service-icon flex-shrink-0">
-                                    <i class="fa fa-home fa-2x"></i>
+                                    <i class="fas fa-chart-line" style="font-size: 1.5rem"></i>
                                 </div>
                                 <h5 class="mb-3">تقييم الأداء</h5>
                                 <p>يقدم إنجاز تقارير دورية حول أداء المهام والمشروع بشكل عام لتمكينك من اتخاذ القرارات الاستراتيجية بناءً على البيانات.</p>
@@ -114,7 +114,7 @@
                         <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
                             <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                                 <div class="service-icon flex-shrink-0">
-                                    <i class="fa fa-home fa-2x"></i>
+                                    <i class="fas fa-sitemap" style="font-size: 1.5rem"></i>
                                 </div>
                                 <h5 class="mb-3">تخصيص مهام الفريق</h5>
                                 <p>يمكن لفريق العمل تخصيص مهامه بشكل فعّال باستخدام أدوات التعيين والتحديد المتاحة في إنجاز لتحقيق أقصى قدر من التنظيم.</p>
@@ -124,7 +124,7 @@
                         <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
                             <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                                 <div class="service-icon flex-shrink-0">
-                                    <i class="fa fa-home fa-2x"></i>
+                                    <i class="fas fa-headset" style="font-size: 1.5rem"></i>
                                 </div>
                                 <h5 class="mb-3">دعم العملاء</h5>
                                 <p>نحن هنا لتقديم دعم كامل لكل عميل، حيث يمكنك الوصول إلينا في أي وقت لحل المشاكل والاستفسارات.</p>
@@ -138,7 +138,7 @@
 
 
         <!-- Portfolio Start -->
-            <div class="container-xxl py-5">
+            {{-- <div class="container-xxl py-5">
                 <div class="container px-lg-5">
                     <div class="section-title position-relative text-center mb-5 pb-2 wow fadeInUp" data-wow-delay="0.1s">
                         <h6 class="position-relative d-inline edit-text-primary pe-4">مشاريعنا</h6>
@@ -228,7 +228,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         <!-- Portfolio End -->
 
 

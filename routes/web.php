cfd66e2ddc908df583880eaf9c20116cc1e9b0e9
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-02-16 00:56:38 +0300

    recognize route P1

--- routes/web.php
+++ routes/web.php
@@ -24,42 +24,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => ['auth', 'checkUserId']], function () {
-Route::get('/dashboard/{userId}', [BoardController::class, 'index'])->name('dashboard.index');
-Route::get('/dashboard/{userId}/lists/{board_id}', [BoardController::class, 'show'])->name('dashboard.lists');
-Route::get('/dashboard/{userId}/card/{card_id}', [CardController::class, 'index'])->name('dashboard.getCardDetails');
-Route::post('/dashboard/{userId}/cardAssigned/add', [CardAssignedController::class, 'addCardAssigned'])->name('dashboard.addCardAssigned');
-Route::post('/dashboard/{userId}/cardAssigned/delete', [CardAssignedController::class, 'deleteCardAssigned'])->name('dashboard.deleteCardAssigned');
-Route::post('/dashboard/{userId}/card/create', [CardController::class, 'store'])->name('dashboard.createCard');
-Route::post('/dashboard/{userId}/card/update', [CardController::class, 'update'])->name('dashboard.updateCard');
-Route::post('/dashboard/{userId}/list/create', [BoardListController::class, 'store'])->name('dashboard.createList');
-Route::post('/dashboard/{userId}/comment/create', [CardCommentController::class, 'store'])->name('dashboard.createComment');
-Route::get('/dashboard/{userId}/users/getAll', [UserController::class, 'getAll'])->name('dashboard.getAllUsers');
-Route::post('/dashboard/{userId}/board/create', [BoardController::class, 'store'])->name('dashboard.createBoard');
-Route::get('/dashboard/{userId}/notification/getAll', [NotificationController::class, 'getAll'])->name('getAllUserNotification');
-Route::get('/dashboard/{userId}/notification/getNew', [NotificationController::class, 'getNew'])->name('getNewUserNotification');
-Route::post('/dashboard/{userId}/notification/moveToStack', [NotificationController::class, 'moveToStack'])->name('NotificationMoveToStack');
-Route::get('/dashboard/{userId}/notification/changeReadState', [NotificationController::class, 'changeReadState'])->name('NotificationChangeReadState');
-Route::post('/dashboard/{userId}/notification/updateNotificationState', [NotificationController::class, 'updateNotificationState'])->name('updateNotificationState');
-Route::post('/dashboard/{userId}/notification/deleteNotification', [NotificationController::class, 'delete'])->name('deleteNotification');
-Route::get('/dashboard/{userId}/getUserProfile/local',[UserController::class, 'jsonGetUserProfile'])->name('localGetUserProfile');
-Route::post('/dashboard/{userId}/saveProfile',[UserController::class, 'saveProfile'])->name('saveProfile');
-Route::get('/dashboard/{userId}/getUserBoards/local',[UserController::class, 'jsonGetUserBoards'])->name('localGetUserBoards');
-Route::get('/dashboard/{userId}/getUserCards/local',[UserController::class, 'jsonGetUserCards'])->name('localGetUserCards');
-Route::get('/dashboard/{userId}/getUserProfile', [UserController::class, 'viewGetUserProfile'])->name('viewGetUserProfile');
-Route::get('/dashboard/{userId}/getUserBoards',[UserController::class, 'viewGetUserBoards'])->name('viewGetUserBoards');
-Route::get('/dashboard/{userId}/getUserCards',[UserController::class, 'viewGetUserCards'])->name('viewGetUserCards');
-Route::get('/dashboard/{userId}/account/settings',[AccountSettingsController::class ,'index'])->name('account.settings');
-Route::post('/dashboard/{userId}/account/settings/update', [AccountSettingsController::class, 'update'])->name('account.settings.update');
-Route::get('/dashboard/{userId}/board/generalSettings/{board_id}',[BoardController::class, 'generalSettings'])->name('board.generalSettings');
-Route::get('/dashboard/{userId}/board/boardMembres/{board_id}',[BoardController::class, 'boardMembres'])->name('board.boardMembres');
-Route::post('/dashboard/{userId}/board/generalSettings/update',[BoardController::class, 'updategeneralSettings'])->name('board.updategeneralSettings');
-Route::post('/dashboard/{userId}/board/boardmember/delete',[BoardController::class, 'delelteBoardMember'])->name('board.delelteBoardMember');
-Route::get('/dashboard/{userId}/users/getUninvite/{board_id}',[UserController::class, 'getUninvite'])->name('board.getUninvite');
-Route::Post('/dashboard/{userId}/board/sendInvitation',[BoardController::class, 'sendInvitation'])->name('board.sendInvitation');
-Route::Post('/dashboard/{userId}/board/deleteForMe',[BoardController::class, 'deleteForMe'])->name('board.deleteForMe');
-Route::Post('/dashboard/{userId}/card/updatePosition',[CardController::class, 'updatePosition'])->name('card.updatePosition');
-Route::Post('/dashboard/{userId}/card/updateList',[CardController::class, 'updateList'])->name('card.updateList');
+Route::group(['middleware' => ['auth', 'checkUserId'],'prefix'=>'dashboard/{userId}'], function () {
+Route::get('',[BoardController::class, 'index'])->name('dashboard.index');
+Route::get('lists/{board_id}', [BoardController::class, 'show'])->name('dashboard.lists');
+Route::get('card/{card_id}', [CardController::class, 'index'])->name('dashboard.getCardDetails');
+Route::post('cardAssigned/add', [CardAssignedController::class, 'addCardAssigned'])->name('dashboard.addCardAssigned');
+Route::post('cardAssigned/delete', [CardAssignedController::class, 'deleteCardAssigned'])->name('dashboard.deleteCardAssigned');
+Route::post('card/create', [CardController::class, 'store'])->name('dashboard.createCard');
+Route::post('card/update', [CardController::class, 'update'])->name('dashboard.updateCard');
+Route::post('list/create', [BoardListController::class, 'store'])->name('dashboard.createList');
+Route::post('comment/create', [CardCommentController::class, 'store'])->name('dashboard.createComment');
+Route::get('users/getAll', [UserController::class, 'getAll'])->name('dashboard.getAllUsers');
+Route::post('board/create', [BoardController::class, 'store'])->name('dashboard.createBoard');
+Route::get('notification/getAll', [NotificationController::class, 'getAll'])->name('getAllUserNotification');
+Route::get('notification/getNew', [NotificationController::class, 'getNew'])->name('getNewUserNotification');
+Route::post('notification/moveToStack', [NotificationController::class, 'moveToStack'])->name('NotificationMoveToStack');
+Route::get('notification/changeReadState', [NotificationController::class, 'changeReadState'])->name('NotificationChangeReadState');
+Route::post('notification/updateNotificationState', [NotificationController::class, 'updateNotificationState'])->name('updateNotificationState');
+Route::post('notification/deleteNotification', [NotificationController::class, 'delete'])->name('deleteNotification');
+Route::get('getUserProfile/local',[UserController::class, 'jsonGetUserProfile'])->name('localGetUserProfile');
+Route::post('saveProfile',[UserController::class, 'saveProfile'])->name('saveProfile');
+Route::get('getUserBoards/local',[UserController::class, 'jsonGetUserBoards'])->name('localGetUserBoards');
+Route::get('getUserCards/local',[UserController::class, 'jsonGetUserCards'])->name('localGetUserCards');
+Route::get('getUserProfile', [UserController::class, 'viewGetUserProfile'])->name('viewGetUserProfile');
+Route::get('getUserBoards',[UserController::class, 'viewGetUserBoards'])->name('viewGetUserBoards');
+Route::get('getUserCards',[UserController::class, 'viewGetUserCards'])->name('viewGetUserCards');
+Route::get('account/settings',[AccountSettingsController::class ,'index'])->name('account.settings');
+Route::post('account/settings/update', [AccountSettingsController::class, 'update'])->name('account.settings.update');
+Route::get('board/generalSettings/{board_id}',[BoardController::class, 'generalSettings'])->name('board.generalSettings');
+Route::get('board/boardMembres/{board_id}',[BoardController::class, 'boardMembres'])->name('board.boardMembres');
+Route::post('board/generalSettings/update',[BoardController::class, 'updategeneralSettings'])->name('board.updategeneralSettings');
+Route::post('board/boardmember/delete',[BoardController::class, 'delelteBoardMember'])->name('board.delelteBoardMember');
+Route::get('users/getUninvite/{board_id}',[UserController::class, 'getUninvite'])->name('board.getUninvite');
+Route::Post('board/sendInvitation',[BoardController::class, 'sendInvitation'])->name('board.sendInvitation');
+Route::Post('board/deleteForMe',[BoardController::class, 'deleteForMe'])->name('board.deleteForMe');
+Route::Post('card/updatePosition',[CardController::class, 'updatePosition'])->name('card.updatePosition');
+Route::Post('card/updateList',[CardController::class, 'updateList'])->name('card.updateList');
 });
 
 Route::get('/dashboard/board', function (){
@@ -106,7 +106,5 @@ Route::get('/contact',function(){
 Route::get('/terms',function(){
     return view('gustes.conditionAndTerms');
 })->name('gustes.terms');
-
-Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');

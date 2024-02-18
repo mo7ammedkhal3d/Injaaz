976c647837b30566b9a49204d3950229454e2c8c
Mo'Khaled Bin-Hazem <Occ716528@gmail.com>
2024-02-19 02:31:15 +0300

    edit authintication

---
+++ lang/ar/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return
+[
+    'accepted' => 'يجب عليك الموافقة على :attribute',
+    'required' => 'هذا الحقل مطلوب',
+    'string' => 'The :attribute field must be a نصا.',
+    'unique' => 'The :attribute has already been taken.',
+];
